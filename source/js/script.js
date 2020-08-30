e7399662863a92e588535689bb47ee7c618fbc8b
Dmitry <bigvolkovski@gmail.com>
2020-08-30 13:34:26 +0300

    Добавлена картя Яндекс API

--- source/js/script.js
+++ source/js/script.js
@@ -4,21 +4,16 @@ var navToggle = document.querySelector(".main-nav__toggle");
 navMain.classList.remove("main-nav--nojs");
 
 navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-  } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
-  }
+  navMain.classList.toggle("main-nav--closed");
+  navMain.classList.toggle("main-nav--opened");
 });
 
 var btnBuy = document.querySelectorAll(".button-buy");
 var modal = document.querySelector(".modal");
 var modalOverlay = document.querySelector(".modal__overlay");
 
-Array.from(btnBuy).forEach(linkbuy => {
-  linkbuy.addEventListener("click", function (evt) {
+Array.from(btnBuy).forEach(linkBuy => {
+  linkBuy.addEventListener("click", function (evt) {
     evt.preventDefault();
     modal.classList.add("modal--active");
   })
@@ -38,3 +33,32 @@ modalOverlay.addEventListener("click", function () {
     modal.classList.remove("modal--active");
   }
 })
+
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map("map", {
+    center: [59.93863506417266, 30.323117499999945],
+    zoom: 16
+  }, {
+    searchControlProvider: "yandex#search"
+  }),
+
+    myPlacemark = new ymaps.Placemark([59.938615082985, 30.32293205597093], {
+      hintContent: "Интернет-магазин Mishka",
+      balloonContent: "Интернет-магазин Mishka, ул. Большая Конюшенная, д.19/8, офис 101"
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#image",
+      // Своё изображение иконки метки.
+      iconImageHref: "img/icon-map-pin.svg",
+      // Размеры метки.
+      iconImageSize: [67, 100],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-20, -100]
+    })
+
+  myMap.geoObjects
+    .add(myPlacemark)
+});
